1e54f31673caab003b3ad2f9ebb53aeb6d1f1922
Jeffrey D. King <jeff.king@weathersource.com>
2018-12-28 13:59:50 -0800

    Added 32 bit alternatives

--- roundp.go
+++ roundp.go
@@ -6,6 +6,12 @@ import (
 
 // Roundp rounds f to n decimal points of precision
 func Roundp(f float64, n int) float64 {
-	pow := math.Pow(10, float64(n))
-	return float64(math.Round(f*pow)) / pow
+	pow := math.Pow10(n)
+	return math.Round(f*pow) / pow
+}
+
+// Roundp32 rounds f to n decimal points of precision
+func Roundp32(f float32, n int) float32 {
+	pow := math.Pow10(n)
+	return float32(math.Round(float64(f)*pow) / pow)
 }
