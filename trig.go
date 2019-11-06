936408c24116f86e36d32cb2be2f8686cd2cbf48
Jeffrey D. King <jeff.king@weathersource.com>
2019-11-06 15:09:33 -0800

    Added trig functions

---
+++ trig.go
@@ -0,0 +1,50 @@
+package mathext
+
+import (
+	"math"
+)
+
+// Deg2Rad converts a degrees to randian
+func Deg2Rad(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
+
+// Rad2Deg converts a radians to degrees
+func Rad2Deg(radians float64) float64 {
+	return radians * 180 / math.Pi
+}
+
+// Sind is the sin trigonometric function expecting x in degrees
+func Sind(x float64) float64 {
+	return math.Sin(Deg2Rad(x))
+}
+
+// Cosd is the cos trigonometric function expecting x in degrees
+func Cosd(x float64) float64 {
+	return math.Cos(Deg2Rad(x))
+}
+
+// Tand is the tan trigonometric function expecting x in degrees
+func Tand(x float64) float64 {
+	return math.Tan(Deg2Rad(x))
+}
+
+// Asind is the asin trigonometric function returning result in degrees
+func Asind(x float64) float64 {
+	return Rad2Deg(math.Asin(x))
+}
+
+// Asind is the acos trigonometric function returning result in degrees
+func Acosd(x float64) float64 {
+	return Rad2Deg(math.Acos(x))
+}
+
+// Atand is the atan trigonometric function returning result in degrees
+func Atand(x float64) float64 {
+	return Rad2Deg(math.Atan(x))
+}
+
+// Atan2d is the atan2 trigonometric function returning result in degrees
+func Atan2d(y, x float64) float64 {
+	return Rad2Deg(math.Atan2(y, x))
+}
---
+++ trig_test.go
@@ -0,0 +1,297 @@
+package mathext
+
+import (
+	"math"
+	"testing"
+
+	assert "github.com/stretchr/testify/assert"
+)
+
+func TestDeg2Rad(t *testing.T) {
+	tests := []struct {
+		value     float64
+		precision int
+		result    float64
+	}{
+		{
+			value:  -180,
+			result: -1 * math.Pi,
+		},
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  90,
+			result: math.Pi / 2,
+		},
+		{
+			value:  180,
+			result: math.Pi,
+		},
+		{
+			value:  270,
+			result: (3 * math.Pi) / 2,
+		},
+		{
+			value:  360,
+			result: 2 * math.Pi,
+		},
+		{
+			value:  540,
+			result: 3 * math.Pi,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Deg2Rad(test.value))
+	}
+}
+
+func TestRad2Deg(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  -1 * math.Pi,
+			result: -180,
+		},
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  math.Pi / 2,
+			result: 90,
+		},
+		{
+			value:  math.Pi,
+			result: 180,
+		},
+		{
+			value:  (3 * math.Pi) / 2,
+			result: 270,
+		},
+		{
+			value:  2 * math.Pi,
+			result: 360,
+		},
+		{
+			value:  3 * math.Pi,
+			result: 540,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Rad2Deg(test.value))
+	}
+}
+
+func TestSind(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  90,
+			result: 1,
+		},
+		{
+			value:  180,
+			result: 0,
+		},
+		{
+			value:  270,
+			result: -1,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Sind(test.value), 10))
+	}
+}
+
+func TestCosd(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 1,
+		},
+		{
+			value:  90,
+			result: 0,
+		},
+		{
+			value:  180,
+			result: -1,
+		},
+		{
+			value:  270,
+			result: 0,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Cosd(test.value), 10))
+	}
+}
+
+func TestTand(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  45,
+			result: 1,
+		},
+		{
+			value:  135,
+			result: -1,
+		},
+		{
+			value:  180,
+			result: 0,
+		},
+		{
+			value:  225,
+			result: 1,
+		},
+		{
+			value:  315,
+			result: -1,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Tand(test.value), 10))
+	}
+}
+
+func TestAsind(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  1,
+			result: 90,
+		},
+		{
+			value:  -1,
+			result: -90,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Asind(test.value), 10))
+	}
+}
+
+func TestAcosd(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 90,
+		},
+		{
+			value:  1,
+			result: 0,
+		},
+		{
+			value:  -1,
+			result: 180,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Acosd(test.value), 10))
+	}
+}
+
+func TestAtand(t *testing.T) {
+	tests := []struct {
+		value  float64
+		result float64
+	}{
+		{
+			value:  0,
+			result: 0,
+		},
+		{
+			value:  1,
+			result: 45,
+		},
+		{
+			value:  -1,
+			result: -45,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Atand(test.value), 10))
+	}
+}
+
+func TestAtan2d(t *testing.T) {
+	tests := []struct {
+		y      float64
+		x      float64
+		result float64
+	}{
+		{
+			y:      0,
+			x:      0,
+			result: 0,
+		},
+		{
+			y:      1,
+			x:      0,
+			result: 90,
+		},
+		{
+			y:      -1,
+			x:      0,
+			result: -90,
+		},
+		{
+			y:      0,
+			x:      1,
+			result: 0,
+		},
+		{
+			y:      1,
+			x:      1,
+			result: 45,
+		},
+		{
+			y:      1,
+			x:      -1,
+			result: 135,
+		},
+		{
+			y:      -1,
+			x:      1,
+			result: -45,
+		},
+		{
+			y:      -1,
+			x:      -1,
+			result: -135,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, Roundp(Atan2d(test.y, test.x), 10))
+	}
+}
