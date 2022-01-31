07e2cbce880ed1a5193cec24dbd2053a96fd3194
Jeffrey D. King <jeff.king@weathersource.com>
2022-01-31 14:43:25 -0800

    added 32 bit support to degree trig functions

--- trig.go
+++ trig.go
@@ -16,35 +16,70 @@ func Rad2Deg(radians float64) float64 {
 
 // Sind is the sin trigonometric function expecting x in degrees
 func Sind(x float64) float64 {
-	return math.Sin(Deg2Rad(x))
+    return math.Sin(Deg2Rad(x))
 }
 
 // Cosd is the cos trigonometric function expecting x in degrees
 func Cosd(x float64) float64 {
-	return math.Cos(Deg2Rad(x))
+    return math.Cos(Deg2Rad(x))
 }
 
 // Tand is the tan trigonometric function expecting x in degrees
 func Tand(x float64) float64 {
-	return math.Tan(Deg2Rad(x))
+    return math.Tan(Deg2Rad(x))
 }
 
 // Asind is the asin trigonometric function returning result in degrees
 func Asind(x float64) float64 {
-	return Rad2Deg(math.Asin(x))
+    return Rad2Deg(math.Asin(x))
 }
 
 // Asind is the acos trigonometric function returning result in degrees
 func Acosd(x float64) float64 {
-	return Rad2Deg(math.Acos(x))
+    return Rad2Deg(math.Acos(x))
 }
 
 // Atand is the atan trigonometric function returning result in degrees
 func Atand(x float64) float64 {
-	return Rad2Deg(math.Atan(x))
+    return Rad2Deg(math.Atan(x))
 }
 
 // Atan2d is the atan2 trigonometric function returning result in degrees
 func Atan2d(y, x float64) float64 {
-	return Rad2Deg(math.Atan2(y, x))
+    return Rad2Deg(math.Atan2(y, x))
+}
+
+// Sind32 is the sin trigonometric function expecting x in degrees as float32
+func Sind32(x float32) float32 {
+    return float32(Sind(float64(x)))
+}
+
+// Cosd32 is the cos trigonometric function expecting x in degrees as float32
+func Cosd32(x float32) float32 {
+    return float32(Cosd(float64(x)))
+}
+
+// Tand32 is the tan trigonometric function expecting x in degrees as float32
+func Tand32(x float32) float32 {
+    return float32(Tand(float64(x)))
+}
+
+// Asind32 is the asin trigonometric function returning result in degrees as float32
+func Asind32(x float32) float32 {
+    return float32(Asind(float64(x)))
+}
+
+// Asind32 is the acos trigonometric function returning result in degrees as float32
+func Acosd32(x float32) float32 {
+    return float32(Acosd(float64(x)))
+}
+
+// Atand32 is the atan trigonometric function returning result in degrees as float32
+func Atand32(x float32) float32 {
+    return float32(Atand(float64(x)))
+}
+
+// Atan2d32 is the atan2 trigonometric function returning result in degrees as float32
+func Atan2d32(y, x float32) float32 {
+    return float32(Atan2d(float64(y), float64(x)))
 }
--- trig_test.go
+++ trig_test.go
@@ -87,211 +87,421 @@ func TestRad2Deg(t *testing.T) {
 }
 
 func TestSind(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 0,
-		},
-		{
-			value:  90,
-			result: 1,
-		},
-		{
-			value:  180,
-			result: 0,
-		},
-		{
-			value:  270,
-			result: -1,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Sind(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  90,
+            result: 1,
+        },
+        {
+            value:  180,
+            result: 0,
+        },
+        {
+            value:  270,
+            result: -1,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Sind(test.value), 10))
+    }
 }
 
 func TestCosd(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 1,
-		},
-		{
-			value:  90,
-			result: 0,
-		},
-		{
-			value:  180,
-			result: -1,
-		},
-		{
-			value:  270,
-			result: 0,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Cosd(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 1,
+        },
+        {
+            value:  90,
+            result: 0,
+        },
+        {
+            value:  180,
+            result: -1,
+        },
+        {
+            value:  270,
+            result: 0,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Cosd(test.value), 10))
+    }
 }
 
 func TestTand(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 0,
-		},
-		{
-			value:  45,
-			result: 1,
-		},
-		{
-			value:  135,
-			result: -1,
-		},
-		{
-			value:  180,
-			result: 0,
-		},
-		{
-			value:  225,
-			result: 1,
-		},
-		{
-			value:  315,
-			result: -1,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Tand(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  45,
+            result: 1,
+        },
+        {
+            value:  135,
+            result: -1,
+        },
+        {
+            value:  180,
+            result: 0,
+        },
+        {
+            value:  225,
+            result: 1,
+        },
+        {
+            value:  315,
+            result: -1,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Tand(test.value), 10))
+    }
 }
 
 func TestAsind(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 0,
-		},
-		{
-			value:  1,
-			result: 90,
-		},
-		{
-			value:  -1,
-			result: -90,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Asind(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  1,
+            result: 90,
+        },
+        {
+            value:  -1,
+            result: -90,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Asind(test.value), 10))
+    }
 }
 
 func TestAcosd(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 90,
-		},
-		{
-			value:  1,
-			result: 0,
-		},
-		{
-			value:  -1,
-			result: 180,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Acosd(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 90,
+        },
+        {
+            value:  1,
+            result: 0,
+        },
+        {
+            value:  -1,
+            result: 180,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Acosd(test.value), 10))
+    }
 }
 
 func TestAtand(t *testing.T) {
-	tests := []struct {
-		value  float64
-		result float64
-	}{
-		{
-			value:  0,
-			result: 0,
-		},
-		{
-			value:  1,
-			result: 45,
-		},
-		{
-			value:  -1,
-			result: -45,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Atand(test.value), 10))
-	}
+    tests := []struct {
+        value  float64
+        result float64
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  1,
+            result: 45,
+        },
+        {
+            value:  -1,
+            result: -45,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Atand(test.value), 10))
+    }
 }
 
 func TestAtan2d(t *testing.T) {
-	tests := []struct {
-		y      float64
-		x      float64
-		result float64
-	}{
-		{
-			y:      0,
-			x:      0,
-			result: 0,
-		},
-		{
-			y:      1,
-			x:      0,
-			result: 90,
-		},
-		{
-			y:      -1,
-			x:      0,
-			result: -90,
-		},
-		{
-			y:      0,
-			x:      1,
-			result: 0,
-		},
-		{
-			y:      1,
-			x:      1,
-			result: 45,
-		},
-		{
-			y:      1,
-			x:      -1,
-			result: 135,
-		},
-		{
-			y:      -1,
-			x:      1,
-			result: -45,
-		},
-		{
-			y:      -1,
-			x:      -1,
-			result: -135,
-		},
-	}
-	for _, test := range tests {
-		assert.Equal(t, test.result, Roundp(Atan2d(test.y, test.x), 10))
-	}
+    tests := []struct {
+        y      float64
+        x      float64
+        result float64
+    }{
+        {
+            y:      0,
+            x:      0,
+            result: 0,
+        },
+        {
+            y:      1,
+            x:      0,
+            result: 90,
+        },
+        {
+            y:      -1,
+            x:      0,
+            result: -90,
+        },
+        {
+            y:      0,
+            x:      1,
+            result: 0,
+        },
+        {
+            y:      1,
+            x:      1,
+            result: 45,
+        },
+        {
+            y:      1,
+            x:      -1,
+            result: 135,
+        },
+        {
+            y:      -1,
+            x:      1,
+            result: -45,
+        },
+        {
+            y:      -1,
+            x:      -1,
+            result: -135,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp(Atan2d(test.y, test.x), 10))
+    }
+}
+
+func TestSind32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  90,
+            result: 1,
+        },
+        {
+            value:  180,
+            result: 0,
+        },
+        {
+            value:  270,
+            result: -1,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Sind32(test.value), 10))
+    }
+}
+
+func TestCosd32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 1,
+        },
+        {
+            value:  90,
+            result: 0,
+        },
+        {
+            value:  180,
+            result: -1,
+        },
+        {
+            value:  270,
+            result: 0,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Cosd32(test.value), 10))
+    }
+}
+
+func TestTand32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  45,
+            result: 1,
+        },
+        {
+            value:  135,
+            result: -1,
+        },
+        {
+            value:  180,
+            result: 0,
+        },
+        {
+            value:  225,
+            result: 1,
+        },
+        {
+            value:  315,
+            result: -1,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Tand32(test.value), 10))
+    }
+}
+
+func TestAsind32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  1,
+            result: 90,
+        },
+        {
+            value:  -1,
+            result: -90,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Asind32(test.value), 10))
+    }
+}
+
+func TestAcosd32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 90,
+        },
+        {
+            value:  1,
+            result: 0,
+        },
+        {
+            value:  -1,
+            result: 180,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Acosd32(test.value), 10))
+    }
+}
+
+func TestAtand32(t *testing.T) {
+    tests := []struct {
+        value  float32
+        result float32
+    }{
+        {
+            value:  0,
+            result: 0,
+        },
+        {
+            value:  1,
+            result: 45,
+        },
+        {
+            value:  -1,
+            result: -45,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Atand32(test.value), 10))
+    }
+}
+
+func TestAtan2d32(t *testing.T) {
+    tests := []struct {
+        y      float32
+        x      float32
+        result float32
+    }{
+        {
+            y:      0,
+            x:      0,
+            result: 0,
+        },
+        {
+            y:      1,
+            x:      0,
+            result: 90,
+        },
+        {
+            y:      -1,
+            x:      0,
+            result: -90,
+        },
+        {
+            y:      0,
+            x:      1,
+            result: 0,
+        },
+        {
+            y:      1,
+            x:      1,
+            result: 45,
+        },
+        {
+            y:      1,
+            x:      -1,
+            result: 135,
+        },
+        {
+            y:      -1,
+            x:      1,
+            result: -45,
+        },
+        {
+            y:      -1,
+            x:      -1,
+            result: -135,
+        },
+    }
+    for _, test := range tests {
+        assert.Equal(t, test.result, Roundp32(Atan2d32(test.y, test.x), 10))
+    }
 }
